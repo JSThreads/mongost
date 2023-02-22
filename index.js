e3ff874ae593fd4a82a3de2d82b12c91e5d00b4a
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-02-22 16:41:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,21 @@ app.post('/user', async (req, res) => {
 
                         try {
                                 let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                let current_perm = current[0].perm.split(';')
-                                if (current.length < 1 || current_perm.find(e => e == ':a') != ':a')
-                                        res.send('Error@user: auth failed\n')
+                                if (current.length == 0)
+                                        res.send('Error@users: auth failed\n')
                                 else {
-                                        // check if the new user already exists
-                                        let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                        if (check.length > 0)
-                                                res.send('Error@user: user already exists\n')
+                                        let current_perm = current[0].perm.split(';')
+                                        if (!current_perm.includes(':a'))
+                                                res.send('Error@user: auth failed\n')
                                         else {
-                                                client.db('api').collection('users').insertOne({ name: req.headers['x-name'], pwd: req.headers['x-pwd'], perm: req.headers['x-perm'] })
-                                                res.send('Success@user: user successfully added\n')
+                                                // check if the new user already exists
+                                                let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
+                                                if (check.length > 0)
+                                                        res.send('Error@user: user already exists\n')
+                                                else {
+                                                        client.db('api').collection('users').insertOne({ name: req.headers['x-name'], pwd: req.headers['x-pwd'], perm: req.headers['x-perm'] })
+                                                        res.send('Success@user: user successfully added\n')
+                                                }
                                         }
                                 }
                         } catch {
@@ -55,21 +59,26 @@ app.delete('/user', async (req, res) => {
                 else {
                         try {
                                 let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                let current_perm = current[0].perm.split(';')
-                                if (current.length < 1 || current_perm.find(e => e == ':a') != ':a')
-                                        res.send('Error@user: auth failed\n')
+                                if (current.length == 0)
+                                        res.send('Error@users: auth failed\n')
                                 else {
-                                        if (req.headers['x-name'] != 'apiAdmin') {
-                                                let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                                if (check.length > 0) {
-                                                        client.db('api').collection('users').deleteOne({ name: req.headers['x-name'] })
-                                                        res.send('Success@user: user successfully deleted\n')
+                                        let current_perm = current[0].perm.split(';')
+                                        if (!current_perm.includes(':a'))
+                                                res.send('Error@user: auth failed\n')
+                                        else {
+                                                if (req.headers['x-name'] != 'apiAdmin') {
+                                                        let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
+                                                        if (check.length > 0) {
+                                                                client.db('api').collection('users').deleteOne({ name: req.headers['x-name'] })
+                                                                res.send('Success@user: user successfully deleted\n')
+                                                        }
+                                                        else
+                                                                res.send('Error@user: user to delete doesn\'t exists\n')
                                                 }
                                                 else
-                                                        res.send('Error@user: user to delete doesn\'t exists\n')
+                                                        res.send('Error@user: apiAdmin user cannot be deleted from the API\n')
+
                                         }
-                                        else
-                                                res.send('Error@user: apiAdmin user cannot be deleted from the API\n')
                                 }
                         } catch {
                                 res.send('Error@user: mongo internal error with users collection\n')
@@ -87,30 +96,34 @@ app.patch('/user', async (req, res) => {
                 else {
                         try {
                                 let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                let current_perm = current[0].perm.split(';')
-                                if (current.length < 1 || current_perm.find(e => e == ':a') != ':a')
-                                        res.send('Error@user: auth failed\n')
+                                if (current.length == 0)
+                                        res.send('Error@users: auth failed\n')
                                 else {
-                                        let new_name_exists = req.headers['x-newname'] != null ? await client.db('api').collection('users').find({ name: req.headers['x-newname'] }).toArray().length > 0 : false
-                                        if (new_name_exists)
-                                                res.send('Error@user: the name you want to change already exists and duplicates aren\'t allowed\n')
+                                        let current_perm = current[0].perm.split(';')
+                                        if (!current_perm.includes(':a'))
+                                                res.send('Error@user: auth failed\n')
                                         else {
-                                                let user_exists = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                                if (user_exists.length > 0) {
-                                                        let new_values = { $set: {  } }
-                                                        if (req.headers['x-newname'] != null)
-                                                                new_values['$set']['name'] = req.headers['x-newname']
-                                                        if (req.headers['x-pwd'] != null)
-                                                                new_values['$set']['pwd'] = req.headers['x-pwd']
-                                                        if (req.headers['x-perm'] != null)
-                                                                new_values['$set']['perm'] = req.headers['x-perm']
+                                                let new_name_exists = req.headers['x-newname'] != null ? await client.db('api').collection('users').find({ name: req.headers['x-newname'] }).toArray().length > 0 : false
+                                                if (new_name_exists)
+                                                        res.send('Error@user: the name you want to change already exists and duplicates aren\'t allowed\n')
+                                                else {
+                                                        let user_exists = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
+                                                        if (user_exists.length > 0) {
+                                                                let new_values = { $set: {  } }
+                                                                if (req.headers['x-newname'] != null)
+                                                                        new_values['$set']['name'] = req.headers['x-newname']
+                                                                if (req.headers['x-pwd'] != null)
+                                                                        new_values['$set']['pwd'] = req.headers['x-pwd']
+                                                                if (req.headers['x-perm'] != null)
+                                                                        new_values['$set']['perm'] = req.headers['x-perm']
 
-                                                        client.db('api').collection('users').updateOne({ name: req.headers['x-name'] }, new_values)
-                                                        res.send('Success@user: user successfully updated\n')
+                                                                client.db('api').collection('users').updateOne({ name: req.headers['x-name'] }, new_values)
+                                                                res.send('Success@user: user successfully updated\n')
 
+                                                        }
+                                                        else
+                                                                res.send('Error@user: the user to update doesn\'t exists\n')
                                                 }
-                                                else
-                                                        res.send('Error@user: the user to update doesn\'t exists\n')
                                         }
                                 }
                         } catch {
@@ -118,11 +131,6 @@ app.patch('/user', async (req, res) => {
                         }
                 }
         }
-})
-
-app.get('/', (req, res) => {
-        client.db('l').collection('test').insertOne({ test: 'test' })
-        res.send('OK\n')
 })
 
 app.listen(port, () => {
