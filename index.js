a7aba011125b6f104d1589f1639af8077d5b0fab
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-02-22 19:18:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,138 +1,97 @@
-require('dotenv').config()
+// setup all configs
+require('dotenv').config();
 
-const { MongoClient } = require('mongodb')
-const client = new MongoClient('mongodb://apiAdmin:admin@localhost:27017/api');
+const { MongoClient } = require('mongodb');
 
-(async () => {
-        await client.connect();
-})();
+const rsaEngine = require('encrypt-rsa').default;
+const ersa = new rsaEngine();
 
-const app = require('express')()
-const port = process.env.PORT
+const app = require('express')();
 
-//              USERS           //
+const port = process.env.PORT;
+let   entrypoint = process.env.ENTRYPOINT;
+const host = process.env.HOST;
+const enrsa = process.env.ENRSA;
+const dersa = process.env.DERSA;
 
-app.post('/user', async (req, res) => {
-        if (req.headers['x-name'] == null || req.headers['x-pwd'] == null || req.headers['x-perm'] == null || req.headers['x-token'] == null)
-                res.send('Error@user: missing header\n')
-        else {
-                let [usr, pwd] = req.headers['x-token'].split('.')
-                if (pwd == null || pwd == '')
-                        res.send('Error@user: wrong token syntax\n')
+if (entrypoint[entrypoint.length - 1] == '/')
+        entrypoint = entrypoint.substring(0, entrypoint.length - 1);
+
+app.post(`${entrypoint}/exec/:database`, async (req, res) => {
+        try {
+                if (req.headers['x-token'] == null)
+                        res.send('Error: Missing headers');
                 else {
-                        // try to get the user..
-                        // if token match and the user has the admin permissions add the new user
-                        // else throw auth error
+                        const token = ersa.decryptStringWithRsaPrivateKey({ text: req.headers['x-token'], dersa });
+                        const [usr, pwd] = token.split('.');
 
-                        try {
-                                let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                if (current.length == 0)
-                                        res.send('Error@users: auth failed\n')
-                                else {
-                                        let current_perm = current[0].perm.split(';')
-                                        if (!current_perm.includes(':a'))
-                                                res.send('Error@user: auth failed\n')
-                                        else {
-                                                // check if the new user already exists
-                                                let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                                if (check.length > 0)
-                                                        res.send('Error@user: user already exists\n')
-                                                else {
-                                                        client.db('api').collection('users').insertOne({ name: req.headers['x-name'], pwd: req.headers['x-pwd'], perm: req.headers['x-perm'] })
-                                                        res.send('Success@user: user successfully added\n')
-                                                }
+                        // url adds on
+                        let db = req.headers['x-database'] || '';
+                        let options = req.headers['x-options'] || '';
+
+                        if (options != '')
+                                options = `?${options}`;
+
+                        const client = new MongoClient(`mongodb://${usr}:${pwd}@${host}/${db}${options}`);
+                        await client.connect();
+
+                        // run command
+                        let cmd = req.headers['x-command'];
+                        const result = await client.db(req.params.database).command(JSON.parse(cmd));
+
+                        await client.close();
+
+                        res.send(JSON.stringify(result));
+                }
+        } catch {
+                await client.close();
+
+                res.send('Error: Something went wrong..');
+        }
+});
+
+app.get(`${entrypoint}/api/:database/:collection`, async (req, res) => {
+        try {
+                if (req.headers['x-token'] == null)
+                        res.send('Error: Missing headers');
+                else if (req.params.database == null || req.params.collection == null)
+                        res.send('Error: Wrong query')
+                else {
+                        const token = ersa.decryptStringWithRsaPrivateKey({ text: req.headers['x-token'], dersa });
+                        const [usr, pwd] = token.split('.');
+
+                        // url adds on
+                        let db = req.headers['x-database'] || '';
+                        let options = req.headers['x-options'] || '';
+
+                        if (options != '')
+                                options = `?${options}`;
+
+                        const client = new MongoClient(`mongodb://${usr}:${pwd}@${host}/${db}${options}`);
+                        await client.connect();
+
+                        // run the query
+                        let query = req.headers['x-query'] || {};
+                        let query_options = req.headers['x-query-options'] || {};
+                        let query_type = req.headers['x-query-type'] || 'mongoDB';     // mongoDB | graphQL
+                        let query_method = req.headers['x-query-method'] || 'find';    // find | aggregate | subscribe | distinct | search => for mongoDB only
+
+                        switch (query_type) {
+                                case 'mongoDB':
+                                        switch (query_method) {
+                                                case 'find':
+                                                break;
+                                                default:
+                                                        res.send('Error: Wrong query method');
                                         }
-                                }
-                        } catch {
-                                res.send('Error@user: mongo internal error with users collection\n')
+                                break;
+                                default:
+                                        res.send('Error: Wrong query type');
                         }
                 }
+        } catch {
+                await client.close();
+
+                res.send('Error: Something went wrong..');
         }
-})
-app.delete('/user', async (req, res) => {
-        if (req.headers['x-name'] == null || req.headers['x-token'] == null)
-                res.send('Error@user: missing header\n')
-        else {
-                let [usr, pwd] = req.headers['x-token'].split('.')
-                if (pwd == null || pwd == '')
-                        res.send('Error@user: wrong token syntax\n')
-                else {
-                        try {
-                                let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                if (current.length == 0)
-                                        res.send('Error@users: auth failed\n')
-                                else {
-                                        let current_perm = current[0].perm.split(';')
-                                        if (!current_perm.includes(':a'))
-                                                res.send('Error@user: auth failed\n')
-                                        else {
-                                                if (req.headers['x-name'] != 'apiAdmin') {
-                                                        let check = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                                        if (check.length > 0) {
-                                                                client.db('api').collection('users').deleteOne({ name: req.headers['x-name'] })
-                                                                res.send('Success@user: user successfully deleted\n')
-                                                        }
-                                                        else
-                                                                res.send('Error@user: user to delete doesn\'t exists\n')
-                                                }
-                                                else
-                                                        res.send('Error@user: apiAdmin user cannot be deleted from the API\n')
-
-                                        }
-                                }
-                        } catch {
-                                res.send('Error@user: mongo internal error with users collection\n')
-                        }
-                }
-        }
-})
-app.patch('/user', async (req, res) => {
-        if (req.headers['x-name'] == null || req.headers['x-token'] == null)
-                res.send('Error@user: missing header\n')
-        else {
-                let [usr, pwd] = req.headers['x-token'].split('.')
-                if (pwd == null || pwd == '')
-                        res.send('Error@user: wrong token syntax\n')
-                else {
-                        try {
-                                let current = await client.db('api').collection('users').find({ name: usr, pwd: pwd }).toArray()
-                                if (current.length == 0)
-                                        res.send('Error@users: auth failed\n')
-                                else {
-                                        let current_perm = current[0].perm.split(';')
-                                        if (!current_perm.includes(':a'))
-                                                res.send('Error@user: auth failed\n')
-                                        else {
-                                                let new_name_exists = req.headers['x-newname'] != null ? await client.db('api').collection('users').find({ name: req.headers['x-newname'] }).toArray().length > 0 : false
-                                                if (new_name_exists)
-                                                        res.send('Error@user: the name you want to change already exists and duplicates aren\'t allowed\n')
-                                                else {
-                                                        let user_exists = await client.db('api').collection('users').find({ name: req.headers['x-name'] }).toArray()
-                                                        if (user_exists.length > 0) {
-                                                                let new_values = { $set: {  } }
-                                                                if (req.headers['x-newname'] != null)
-                                                                        new_values['$set']['name'] = req.headers['x-newname']
-                                                                if (req.headers['x-pwd'] != null)
-                                                                        new_values['$set']['pwd'] = req.headers['x-pwd']
-                                                                if (req.headers['x-perm'] != null)
-                                                                        new_values['$set']['perm'] = req.headers['x-perm']
-
-                                                                client.db('api').collection('users').updateOne({ name: req.headers['x-name'] }, new_values)
-                                                                res.send('Success@user: user successfully updated\n')
-
-                                                        }
-                                                        else
-                                                                res.send('Error@user: the user to update doesn\'t exists\n')
-                                                }
-                                        }
-                                }
-                        } catch {
-                                res.send('Error@user: mongo internal error with users collection\n')
-                        }
-                }
-        }
-})
-
-app.listen(port, () => {
-  console.log(`🥭 Mongo API listening on port ${port}`)
 })
